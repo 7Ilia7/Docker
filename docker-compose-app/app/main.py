2ccfa0d5078f71c0561c839fe77ee07ddec5dfc5
PythonLesson <greench707@gmail.co>
2022-08-28 20:23:45 +0300

    new command

---
+++ docker-compose-app/app/main.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+MONGO_URL = "mongodb://mongo:27017"
+clent = MongoClient(MONGO_URL)
+db = client.admin
+dbs_list = db.comman("listDatabases")
+pprint(dbs_list)
